1899a470ceda018dd4ebe35c2eb8a6dbefe48141
Avery Swafford <averyds17@gmail.com>
2020-12-08 15:57:47 -0500

    Added add to cart button on cards in the card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -16,7 +16,7 @@ export const Card = () => {
                     {item.price}
                 </div>
                 <div className="card-cart">
-
+                    Add to Cart
                 </div>
             </div>
         )}
